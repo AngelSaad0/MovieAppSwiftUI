8aef2a2ef988cd3a7ee66dce0d9fc854c91560e4
AngelSaad0 <engysaad573new@gmail.com>
2024-09-26 01:24:25 +0300

    Initial Commit

---
+++ MovieApp/MovieAppApp.swift
@@ -0,0 +1,20 @@
+//
+//  MovieAppApp.swift
+//  MovieApp
+//
+//  Created by Engy on 9/26/24.
+//
+
+import SwiftUI
+
+@main
+struct MovieAppApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
